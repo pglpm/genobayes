4930565fafbf41df8f088595dfa6f2606d015cbc
luca <piergianluca@gmail.com>
2018-09-14 13:52:14 +0200

    scripting

---
+++ scripts/mutualinfo1.R
@@ -0,0 +1,29 @@
+## Calculation of mutual information from sampled data
+## test 1
+
+## libraries and colour-blind palette from http://www.sron.nl/~pault/
+library('ggplot2')
+library('RColorBrewer')
+library('cowplot')
+library('png')
+library('plot3D')
+library('doParallel')
+library('GA')
+
+mypurpleblue <- '#4477AA'
+myblue <- '#66CCEE'
+mygreen <- '#228833'
+myyellow <- '#CCBB44'
+myred <- '#EE6677'
+myredpurple <- '#AA3377'
+mygrey <- '#BBBBBB'
+mycolours <- c(myblue, myred, mygreen, myyellow, myredpurple, mypurpleblue, mygrey, 'black')
+palette(mycolours)
+barpalette <- colorRampPalette(c(mypurpleblue,'white',myredpurple),space='Lab')
+barpalettepos <- colorRampPalette(c('white','black'),space='Lab')
+dev.off()
+mmtoin <- 0.0393701
+plotsdir <- './'
+
+
+
