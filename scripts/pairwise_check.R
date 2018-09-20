0693c75cc7fd002f5d2a8c027b02556027d77b3a
luca <piergianluca@gmail.com>
2018-09-20 15:41:24 +0200

    scripting

--- scripts/pairwise_check.R
+++ scripts/pairwise_check.R
@@ -3,8 +3,8 @@ source('mutualinfo2.R')
 npairs <- 94*93/2
 
 pb <- txtProgressBar(min = 0, max = 94)
-res <- do.call(cbind,sapply(1:93,function(i){    setTxtProgressBar(pb, i)
-    sapply((i+1):94,function(j){
+res <- do.call(cbind,sapply(1:3,function(i){    setTxtProgressBar(pb, i)
+    sapply((i+1):4,function(j){
         c(i,j,calculatemutualinfo(c(i,j),messages=FALSE)[1])
     })}))
 close(pb)
