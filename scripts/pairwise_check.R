daa4901da4a76e1fe67f475f653299c4c21f2abe
luca-hippo <pglpm0@gmail.com>
2018-09-20 15:41:02 +0200

    update data

--- scripts/pairwise_check.R
+++ scripts/pairwise_check.R
@@ -2,7 +2,7 @@ source('mutualinfo2.R')
 
 npairs <- 94*93/2
 
-pb <- txtProgressBar(min = 0, max = 94)
+pb <- txtProgressBar(min = 0, max = 94,style=3)
 res <- do.call(cbind,sapply(1:93,function(i){    setTxtProgressBar(pb, i)
     sapply((i+1):94,function(j){
         c(i,j,calculatemutualinfo(c(i,j),messages=FALSE)[1])
