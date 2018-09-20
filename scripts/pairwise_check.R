e90a377b5877e9e29ef0f47d07408c89ba54d006
luca <piergianluca@gmail.com>
2018-09-21 00:25:45 +0200

    scripting

--- scripts/pairwise_check.R
+++ scripts/pairwise_check.R
@@ -10,4 +10,11 @@ res <- do.call(cbind,sapply(1:93,function(i){    setTxtProgressBar(pb, i)
 close(pb)
 
 
-saveRDS(res,'results_pairs.rds')
+saveRDS(res,'results_pairs2.rds')
+
+
+## c(6, 9, 17, 21, 30, 31, 32, 34, 38, 42, 46, 51, 53, 57, 58, 59, 61, 64, 66, 68, 69, 70, 72, 75, 78, 83, 88, 89, 90, 92, 93) give  0.00234867116380749
+
+## c(6, 32, 57, 59, 64, 68, 72, 88, 89, 90) give  0.0791343853227863 (4% normalized)
+
+## c(59, 68, 88, 89, 90) give
