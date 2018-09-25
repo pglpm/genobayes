413e9f5dd9d785a8244c8efd3cbe9894c90c4647
luca <piergianluca@gmail.com>
2018-09-25 14:42:56 +0200

    scripting

--- scripts/pairwise_check_dirich.R
+++ scripts/pairwise_check_dirich.R
@@ -5,7 +5,7 @@ npairs <- 94*93/2
 pb <- txtProgressBar(min = 1, max = 93,style=3)
 res <- do.call(cbind,sapply(1:93,function(i){    setTxtProgressBar(pb, i)
     sapply((i+1):94,function(j){
-        c(i,j,calculatemutualinfo(c(i,j),cores=30,messages=FALSE)[1])
+        c(i,j,calculatemutualinfo(c(i,j),cores=1,messages=FALSE)[1])
     })}))
 close(pb)
 
