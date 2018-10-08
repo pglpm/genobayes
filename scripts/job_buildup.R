9e34cfd08b6a6c87dcb95b91427d64f3b2edbb35
luca <piergianluca@gmail.com>
2018-10-08 14:49:08 +0200

    scripting

---
+++ scripts/job_buildup.R
@@ -0,0 +1,1 @@
+source('mutualinfo_buildup.R')
--- scripts/mutualinfo_buildup.R
+++ scripts/mutualinfo_buildup.R
@@ -2,6 +2,7 @@
 ## for 1 gene, 2 genes, etc, keeping at each step those with highest mutual info
 
 ## libraries and colour-blind palette from http://www.sron.nl/~pault/
+##runfunction <- function(aa=1e3L){
 library('ggplot2')
 library('RColorBrewer')
 library('cowplot')
@@ -24,150 +25,130 @@ barpalette <- colorRampPalette(c(mypurpleblue,'white',myredpurple),space='Lab')
 barpalettepos <- colorRampPalette(c('white','black'),space='Lab')
 dev.off()
 mmtoin <- 0.0393701
-plotsdir <- './'
+savedir <- './'
 
 xlogy <- function(x,y){if(x==0){0}else{x*log(y)}}
 
 dpath  <-  "./"
+datafile <- 'dataset1.csv'
 nfile  <-  dir(path = dpath,pattern = datafile)
 d <- read.csv(paste0(dpath,nfile[1]),sep=";")
+predata <- d[#sample(1:dim(d)[1],4000)
+       ,c(6,7+(1:94))]
+predata[,1+(1:94)] <- (predata[,1+(1:94)]>0)*1
+n <- length(predata[,1])
+cs <- length(unique(predata[,1]))
+fs <- data.matrix(tally(group_by_at(predata,.vars=c(1))))
+nn <- 5.3e6L
 
-## this tuple contains the genes kept so far
-keptgenes <- NA
+aa <- 1000
 
-## this tuple contains the genes to be checked in 
-tocheck <- 1:94
+n2 <- n+aa
+dnna <- 1/n2 #(nn-n)/(nn*n2)
 
+## iteration at which to switch parallel loop
 changepar <- 2^7
 
-for(iter=1:94){
+totalgenes <- 1:94
+## this tuple contains the genes kept so far
+keptgenes <- NULL
+keptinfo <- NULL
+## this tuple contains the genes to be checked in 
+genegroup <- totalgenes
 
+cores <- 30
+cl <- makeCluster(cores)
+registerDoParallel(cl)
+
+for(ngenes in totalgenes){
+    message(paste0('iteration ',ngenes))
     ## "if" to choose whether to parallelize outer or inner loop
-    if(iter<changepar){# parallelize outer loop
+    if(ngenes<changepar){# parallelize outer loop
 
-        
+        allminfo <- foreach(i=genegroup, .combine=rbind, .export=c('xlogy','keptgenes','ngenes','n','cs','fs','aa','n2','dnna'),
+                            .packages=c('dplyr','doParallel')) %dopar% {
+            genes <- sort(c(keptgenes,i))
+            data <- predata[,c(1,1+genes)]
+            ## uncomment this to shuffle the symptom data
+            ##    data[,1] <- sample(data[,1])
+            cg <- 2^ngenes
+            cx <- cs*cg
+            ## data frequencies for genes
+            fg <- data.matrix(tally(group_by_at(data,.vars=c(2:(1+ngenes)))))
+            ## data frequencies for both
+            fx <- data.matrix(tally(group_by_at(data,.vars=c(1:(ngenes+1)))))
 
+            cgminus <- cg-dim(fg)[1]
+            cgplus <- dim(fg)[1]
 
+            minfo <- sum(apply(fs,1,function(sig){
+                ##  if(messages==TRUE){print(sig[1])}
+                foreach(i=1:cgplus, .combine=cbind#,
+                                        #  .export=c('fg','fx','ngenes','cg','cs','cx','n','n2','dnna','xlogy','aa')
+                        ) %do% {
+                                        gam <- fg[i,]
+                                        ## marginal freqs
+                                        ns <- sig[2]
+                                        ng <- gam[ngenes+1]
+                                        ## frequency of pair
+                                        nx <- sum(apply(fx,1,function(i){all(i[1:(ngenes+1)]==c(sig[1],gam[1:ngenes]))*i[ngenes+2]}))
+                                        
+                                        probx <- dnna*(nx+aa/cx)
+                                        probs <- dnna*(ns+aa/cs)
+                                        probg <- dnna*(ng+aa/cg)
 
+                                        xlogy(probx,probx/(probs*probg))}
+            })) -
+    cgminus*2*dnna/cx*sum(log((dnna*(fs[,2]+aa/cs))*cx/cg))
+            minfo
+        }
+    } else {# parallelize inner loop
+        allminfo <- foreach(i=genegroup, .combine=rbind #, .export=c('xlogy','keptgenes','ngenes','n','cs','fs','aa','n2','dnna'),
+                           ) %do% {
+            genes <- sort(c(keptgenes,i))
+            data <- predata[,c(1,1+genes)]
+            ## uncomment this to shuffle the symptom data
+            ##    data[,1] <- sample(data[,1])
+            cg <- 2^ngenes
+            cx <- cs*cg
+            ## data frequencies for genes
+            fg <- data.matrix(tally(group_by_at(data,.vars=c(2:(1+ngenes)))))
+            ## data frequencies for both
+            fx <- data.matrix(tally(group_by_at(data,.vars=c(1:(ngenes+1)))))
 
+            cgminus <- cg-dim(fg)[1]
+            cgplus <- dim(fg)[1]
 
+            minfo <- sum(apply(fs,1,function(sig){
+                ##  if(messages==TRUE){print(sig[1])}
+                foreach(i=1:cgplus, .combine=cbind , .export=c('fg','fx','ngenes','cg','cs','cx','n','n2','dnna','xlogy','aa')
+                        ) %dopar% {
+                                        gam <- fg[i,]
+                                        ## marginal freqs
+                                        ns <- sig[2]
+                                        ng <- gam[ngenes+1]
+                                        ## frequency of pair
+                                        nx <- sum(apply(fx,1,function(i){all(i[1:(ngenes+1)]==c(sig[1],gam[1:ngenes]))*i[ngenes+2]}))
+                                        
+                                        probx <- dnna*(nx+aa/cx)
+                                        probs <- dnna*(ns+aa/cs)
+                                        probg <- dnna*(ng+aa/cg)
 
-
-
-
-
+                                        xlogy(probx,probx/(probs*probg))}
+            })) -
+    cgminus*2*dnna/cx*sum(log((dnna*(fs[,2]+aa/cs))*cx/cg))
+            minfo
+        }
     }
-
-
-
-
-
-
-
+    
+        
+    write.table(cbind(genegroup,allminfo),paste0(savedir,'iteration_',ngenes,'.csv'),sep=',',row.names=F,col.names=F,na='Infinity')
+    maxinfo <- max(allminfo)
+    keptinfo <- c(keptinfo,maxinfo)
+    maxgene <- genegroup[which.max(allminfo)]
+    keptgenes <- c(keptgenes,maxgene)
+    genegroup <- totalgenes[-keptgenes]
+    write.table(cbind(keptgenes,keptinfo),paste0(savedir,'infoseq_',ngenes,'.csv'),sep=',',row.names=F,col.names=F,na='Infinity')
 }
+stopCluster(cl)
 
-
-
-calculatemutualinfo <- function(whichgenes,a=2,cores=20,datafile='dataset1',messages=TRUE){
-
-    if(any(whichgenes<1) | any(whichgenes>94)){print('Error: allowed genes within [1,94]')}
-    ## load all data
-
-    ## insomnia symptoms = col 6
-    ## gene data = cols 8:102
-    ## genes can be any subset of {1,...,95}
-    genes <- c(whichgenes)
-    ngenes <- length(genes)
-    data <- d[#sample(1:dim(d)[1],4000)
-       ,c(6,7+genes)]
-    data[,2:dim(data)[2]] <- (data[,2:dim(data)[2]]>0)*1
-    ## uncomment this to shuffle the symptom data
-    ##    data[,1] <- sample(data[,1])
-    l <- length(data[1,])
-    n <- length(data[,1])
-    nn <- 5.3e6L
-    cs <- length(unique(data[,1]))
-    cg <- 2^ngenes
-    cx <- cs*cg
-
-    if(a==0){aa  <- cx}else if(a==-2){aa <- sqrt(2*cx)}else if(a==-10){aa <- nn}else{aa <- a}
-
-    ## data frequencies for symptoms
-    fs <- data.matrix(tally(group_by_at(data,.vars=c(1))))
-    #print(dim(fs))
-    ## data frequencies for genes
-    fg <- data.matrix(tally(group_by_at(data,.vars=c(2:(1+ngenes)))))
-    #print(dim(fg))
-    ## data frequencies for both
-    fx <- data.matrix(tally(group_by_at(data,.vars=c(1:(ngenes+1)))))
-    #print(dim(fx))
-
-    cgminus <- cg-dim(fg)[1]
-    cgplus <- dim(fg)[1]
-
-    n2 <- n+aa
-    dnna <- (nn-n)/(nn*n2)
-    ## minfo <- log(ncnn) + sum(apply(fs,1,function(sig){
-    ##     apply(fg,1,function(gam){
-    ##         ## marginal freqs
-    ##         ns <- sig[2]
-    ##         ng <- gam[ngenes+1]
-    ##         ## frequency of pair
-    ##         nx <- sum(apply(fx,1,function(i){all(i[1:(ngenes+1)]==c(sig[1],gam[1:ngenes]))*i[ngenes+2]}))
-    ##         numer <- nnc*nx+dn
-            
-    ##         numer/ncnn*(log(numer)-log(nnc*ns+dn*cg)-log(nnc*ng+dn*cs))})})) -
-    ##     cgminus*dn/ncnn*sum(log(cs*(nnc*fs[,2]+dn*cg)))
-
-        ## minfo2 <- log(ncnn) + sum(apply(fs,1,function(sig){
-        ## apply(fg,1,function(gam){
-        ##     ## marginal freqs
-        ##     ns <- sig[2]
-        ##     ng <- gam[ngenes+1]
-        ##     ## frequency of pair
-        ##     nx <- sum(apply(fx,1,function(i){all(i[1:(ngenes+1)]==c(sig[1],gam[1:ngenes]))*i[ngenes+2]}))
-        ##     numer <- nnc*nx+dn
-            
-        ##     numer/ncnn*log(numer/((nnc*ns+dn*cg)*(nnc*ng+dn*cs)))})})) -
-        ## cgminus*dn/ncnn*sum(log(cs*(nnc*fs[,2]+dn*cg)))
-
-    cl <- makeCluster(cores)
-    registerDoParallel(cl)
-    minfo <- sum(apply(fs,1,function(sig){
-if(messages==TRUE){print(sig[1])}
-        foreach(i=1:cgplus, .combine=cbind,
-                .export=c('fg','fx','ngenes','cg','cs','cx','nn','n','n2','dnna','xlogy','aa')
-                ) %dopar% {
-            gam <- fg[i,]
-            ## marginal freqs
-            ns <- sig[2]
-            ng <- gam[ngenes+1]
-            ## frequency of pair
-            nx <- sum(apply(fx,1,function(i){all(i[1:(ngenes+1)]==c(sig[1],gam[1:ngenes]))*i[ngenes+2]}))
-
-            probx <- nx/nn+dnna*(nx+aa/cx)
-            probs <- ns/nn+dnna*(ns+aa/cs)
-            probg <- ng/nn+dnna*(ng+aa/cg)
-
-            xlogy(probx,probx/(probs*probg))}})) -
-        cgminus*2*dnna/cx*sum(log((fs[,2]/nn+dnna*(fs[,2]+aa/cs))*cx/cg))
-    stopCluster(cl)
-    
-    
-    sentropy <- -sum(apply(fs,1,function(sig){
-        prob <- (sig[2]/nn+dnna*(sig[2]+aa/cs))
-        xlogy(prob,prob)}))
-if(messages==TRUE){
-    print(paste0('max value = ',sentropy))
-    print(paste0('mutual information = ',minfo))
-    print(paste0('normalized value = ',minfo/sentropy))}
-    c(minfo,sentropy)}
-
-## all genes:
-
-## [1] "max value = 1.73891986550377"
-## [1] "mutual information = 1.73823079314495"
-## [1] "normalized value = 0.999603735415018"
-## There were 20 warnings (use warnings() to see them)
-
-## "best genes" from pairwise analysis: {{32, 2}, {66, 2}, {6, 4}, {57, 1}, {75, 3}, {9, 1}, {58, 1}, {89, 1}, {53, 1}, {21, 1}, {56, 1}, {42, 1}, {68, 1}}
