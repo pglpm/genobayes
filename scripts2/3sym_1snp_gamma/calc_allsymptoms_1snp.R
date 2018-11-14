dabf63b3d16844ec2b87a7a714543eb08de2eda7
luca <piergianluca@gmail.com>
2018-11-14 03:07:20 +0100

    scripting

--- scripts2/3sym_1snp_gamma/calc_allsymptoms_1snp.R
+++ scripts2/3sym_1snp_gamma/calc_allsymptoms_1snp.R
@@ -65,8 +65,8 @@ namesnpvariants <- c('0','1') # allele names
 
 ## log-prior for thetas: see research notes
 ## 'lt' is the log of theta
-#logpriortheta <- function(lt){dcauchy(lt,location=log(1000),scale=log(1000),log=TRUE)-lt}
-logpriortheta <- function(lt,t){dgamma(sum(t),shape=1,scale=1000,log=TRUE)-log(sum(t))}
+#logpriortheta <- function(lt,t){sum(dcauchy(lt,location=log(1000),scale=log(1000),log=TRUE))-sum(lt)}
+logpriortheta <- function(lt,t){dgamma(sum(t),shape=1,scale=1000,log=TRUE)-7*log(sum(t))}
 
 ## measure of spread, applied to the final matrix of quantities
 ## it calculates abs((EV_freq1 - EV_freq2)/(SD_freq1 + SD_freq2))
